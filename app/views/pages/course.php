1e19ba9c8b427e57aef9193ac87631147215e4d9
Xuan Son <soncx@Xuans-MacBook-Pro.local>
2021-11-05 23:51:02 +0700

    dev trang thong tin khoa hoc

--- app/views/pages/course.php
+++ app/views/pages/course.php
@@ -4,17 +4,356 @@
 <body>
     <?php $this->View("navigation");?>
 
-     <!-- Code HTML ở đây  -->
-    <h1>Sơ bộ khóa học</h1>
-
-
-
-
-
+    <div id="main">
+        <div class="grid">
+            <div class="grid__row">
+                <div class="grid__column-8">
+                    <div class="course-content">
+                        <div class="course-info">
+                            <div class="course-summary">
+                                <h1 class="course-summary-title">Node & ExpressJS</h1>
+                                <p class="course-summary-intro">
+                                    Học Back-end với Node & ExpressJS framework, hiểu các khái niệm khi làm Back-end và xây dựng RESTful API cho trang web.
+                                </p>
+                            </div>
+                            <div class="course-gain">
+                                <h4 class="course-gain-title">Bạn sẽ học được gì</h4>
+                                <div class="course-gain-list">
+                                    <ul class="fa-ul">
+                                        <li><i class="fa-li fa fa-check"></i>Nắm chắc lý thuyết chung trong việc xây dựng web</li>
+                                        <li><i class="fa-li fa fa-check"></i>Xây dựng web với Express bằng kiến thức thực tế</li>
+                                        <li><i class="fa-li fa fa-check"></i>Nắm chắc lý thuyết về API và RESTful API</li>
+                                        <li><i class="fa-li fa fa-check"></i>Nắm chắc khái niệm về giao thức HTTP</li>
+                                        <li><i class="fa-li fa fa-check"></i>Học được cách tổ chức code trong thực tế</li>
+                                        <li><i class="fa-li fa fa-check"></i>Biết cách làm việc với Mongoose, MongoDB trong NodeJS</li>
+                                        <li><i class="fa-li fa fa-check"></i>Biết cách xây dựng API theo chuẩn RESTful API</li>
+                                        <li><i class="fa-li fa fa-check"></i>Được chia sẻ lại kinh nghiệm làm việc thực tế</li>
+                                        <li><i class="fa-li fa fa-check"></i>Hiểu rõ tư tưởng và cách hoạt động của mô hình MVC</li>
+                                        <li><i class="fa-li fa fa-check"></i>Biết cách deploy (triển khai) website lên internet</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="course-detail">
+                            <h3 class="course-detail-title">Nội dung khóa học</h3>
+                            <ul class="course-detail-summary">
+                                <li>3 phần</li>
+                                <li class="divider">.</li>
+                                <li>36 bài học</li>
+                                <li class="divider">.</li>
+                                <li>Thời lượng 12 giờ 08 phút</li>
+                            </ul>
+                            <div class="course-detail-list">
+                                <div class="list">
+                                    <div class="list-title">
+                                        <span class="main-title">1. Bắt đầu</span>
+                                        <span class="video-count">8 bài học</span>
+                                    </div>
+                                    <div class="list-collapse">
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>1. Lời khuyên trước khóa học</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>2. HTTP protocol</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>3. SSR & CSR</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>4. Install Node</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>5. Install ExpressJS</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>6. Install Nodemon & inspector</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>7. Add git repo</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>8. Install Morgan</div>
+                                    </div>
+                                </div>
+                                <div class="list">
+                                    <div class="list-title">
+                                        <span class="main-title">2. Kiến thức cốt lõi</span>
+                                        <span class="video-count">8 bài học</span>
+                                    </div>
+                                    <div class="list-collapse">
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>9. Template engine (handlebars)</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>10. Static file & SCSS</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>11. Use Bootstrap</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>12. Basic routing</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>13. GET method</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>14. Query parameters</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>15. Form default behavior</div>
+                                        <div class="collapse-item"><i class="fas fa-play-circle"></i>16. POST method</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="course-require">
+                            <h3 class="require-title">Yêu cầu</h3>
+                            <ul class="fa-ul">
+                                <li><i class="fa-li fa fa-check"></i>Có kiến thức cơ bản về HTML, CSS và Javascript</li>
+                                <li><i class="fa-li fa fa-check"></i>Nắm chắc các tính năng trong Javascript ES6</li>
+                                <li><i class="fa-li fa fa-check"></i>Có hiểu biết về lập trình bất đồng bộ trong Javascript</li>
+                                <li><i class="fa-li fa fa-check"></i>Sở hữu máy tính kết nối internet HDH Windows, Ubuntu hoặc MacOS</li>
+                                <li><i class="fa-li fa fa-check"></i>Ý thức cao, trách nhiệm cao trong việc tự học. Thực hành lại sau mỗi bài học</li>
+                                <li><i class="fa-li fa fa-check"></i>Khi học nếu có khúc mắc hãy tham gia hỏi/đáp tại group FB: Học lập trình web (fullstack.edu.vn)</li>
+                                <li><i class="fa-li fa fa-check"></i>Bạn không cần biết gì hơn nữa, trong khóa học tôi sẽ chỉ cho bạn những gì bạn cần phải biết</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid__column-4">
+                    <div class="course-purchase">
+                        <div class="course-thumb">
+                            <img src="<?php echo URL_ROOT ?>/public/assets/img/course/6.png" alt="course-thumb" class="center">
+                            <i class="fas fa-play-circle"></i>
+                            <p>Xem giới thiệu khóa học</p>
+                        </div>
+                        <h5 class="course-fee">Miễn phí</h5>
+                        <button class="course-btn">ĐĂNG KÝ HỌC</button>
+                        <ul>
+                            <li>
+                                <i class="fas fa-seedling"></i>
+                                <span>Trình độ cơ bản</span>
+                            </li>
+                            <li>
+                                <i class="fas fa-film"></i>
+                                <span>
+                                    Tổng số <strong>36</strong> bài học
+                                </span>
+                            </li>
+                            <li>
+                                <i class="fas fa-clock"></i>
+                                <span>
+                                    Thời lượng <strong>12 giờ 08 phút</strong>
+                                </span>
+                            </li>
+                            <li>
+                                <i class="fas fa-battery-full"></i>
+                                <span>Học mọi lúc mọi nơi</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer"></div>
+     
     <?php $this->View("footer");?>
     <style>
         /* Code CSS ở đây */
-        
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        html, body {
+        scroll-behavior: smooth;
+        font-size: 15px;
+        background-color: #f1f1f1;
+        }
+
+        /* COMMON */
+        ul{
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+        padding: 0;
+        margin-bottom: 0;
+        }
+        li{
+        padding-bottom: 12px;
+        font-size: 14px;
+        }
+        li i{
+        color: #f05122;
+        font-size: 16px;
+        }
+        .center{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        }
+        .mt-16{
+        margin-top: 16px;
+        }
+        .mb-16{
+        margin-bottom: 16px;
+        }
+
+        /* RESPONSIVE */
+        .grid{
+        width: 1300px;
+        max-width: 100%;
+        margin: 0 auto;
+        }
+        .grid__full-width{
+        width: 100%;
+        }
+        .grid__row{
+        display: flex;
+        flex-wrap: wrap;
+        margin-left: -12px;
+        margin-right: -12px;
+        }
+        .grid__column-4{
+        padding-left: 12px;
+        padding-right: 12px;
+        width: calc(100% / 12 * 4);
+        position: relative;
+        }
+        .grid__column-8{
+        padding-left: 12px;
+        padding-right: 12px;
+        width: calc(100% / 12 * 8);
+        }
+
+        /* MAIN */
+        #main{
+        padding-top: 30px;
+        }
+        #main .course-summary-title{
+        font-size: 32px;
+        font-weight: 700;
+        padding-bottom: 16px;
+        }
+        #main .course-summary-intro{
+        font-size: 14px;
+        padding-bottom: 16px;
+        }
+        #main .course-gain-title{
+        font-size: 20px;
+        font-weight: 700;
+        padding-bottom: 10px;
+        }
+        #main .course-gain-list ul{
+        flex-direction: column;
+        padding-left: 24px;
+        margin: 0;
+        }
+        #main .course-detail-title{
+        font-weight: 700;
+        font-size: 20px;
+        padding-top: 14px;
+        padding-bottom: 8px;
+        }
+        #main .course-detail-summary{
+        margin-bottom: 0;
+        display: flex;
+        flex-direction: row;
+        }
+        #main .course-detail-summary li{
+        font-size: 14px;
+        justify-content: space-around;
+        padding-right: 8px;
+        }
+        #main .course-detail-summary .divider{
+        font-weight: 700;
+        text-align: center;
+        }
+        #main .list{
+        cursor: pointer;
+        }
+        #main .list-title,
+        #main .list-title:before{
+        height: 50px;
+        border-radius: 4px;
+        }
+        #main .list-title{
+        font-weight: 600;
+        background-color: #fbf8f8;
+        padding: 14px 30px 14px 48px;
+        position: relative;
+        }
+        #main .list-title:before{
+        content: "-";
+        font-size: 40px;
+        position: absolute;
+        left: 14px;
+        top: -8px;
+        color: #f05122;
+        }
+        #main .list-title .video-count{
+        position: absolute;
+        right: 30px;
+        font-weight: 300;
+        }
+        #main .collapse-item{
+        padding-top: 16px;
+        padding-bottom: 16px;
+        padding-left: 32px;
+        font-size: 14px;
+        border-top: 1px solid #fbf8f8;
+        border-bottom: 1px solid #fbf8f8;
+        }
+        #main .collapse-item i{
+        padding-right: 12px;
+        color: #f05122;
+        }
+
+        #main .course-require .require-title{
+        font-size: 20px;
+        font-weight: 700;
+        margin-top: 36px;
+        margin-bottom: 14px;
+        }
+        #main .course-require ul{
+        flex-direction: column;
+        padding-left: 24px;
+        margin: 0;
+        }
+
+        #main .course-purchase{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        line-height: 11.5px;
+        position: fixed;
+        }
+        #main .course-thumb{
+        position: relative;
+        cursor: pointer;
+        }
+        #main .course-thumb i{
+        position: absolute;
+        font-size: 54px;
+        color: #f1f1f1;
+        position: absolute;
+        top: 40%;
+        left: 44%;
+        }
+        #main .course-thumb p{
+        font-weight: 600;
+        color: #f1f1f1;
+        position: absolute;
+        top: 85%;
+        left: 30%;
+        }
+        #main .course-fee{
+        color: #f05123;
+        font-size: 32px;
+        font-weight: 300;
+        padding-top: 24px;
+        padding-bottom: 14px;
+        }
+        #main .course-btn{
+        border: none;
+        padding: 1px 18px;
+        border-radius: 14px;
+        align-items: flex-start;
+        background-color:#f05123;
+        color:#ffffff;
+        /* font-family: Montserrat; */
+        letter-spacing: 1.5px;
+        line-height: 40px;
+        text-align: center;
+        text-transform: uppercase;
+        }
+        #main .course-purchase ul{
+        flex-direction: column;
+        margin-top: 32px;
+        }
+        #main .course-purchase ul i{
+        color: #494949;
+        }
+        #main .course-purchase ul span{
+        padding-left: 18px;
+        }
+        #main .course-thumb img{
+        width: 98%;
+        filter: brightness(0.7);
+        }
     </style>
 </body>
 </html>
